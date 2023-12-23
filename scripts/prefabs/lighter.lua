dfa4f0ad2874b42c07501b1302381f2d6602375a
Peng <pengsu666@gmail.com>
2023-12-23 11:43:11 +0800

    follow update

--- scripts/prefabs/lighter.lua
+++ scripts/prefabs/lighter.lua
@@ -8,7 +8,159 @@ local assets =
 local prefabs =
 {
     "lighterfire",
+	"channel_absorb_fire_fx",
+    "channel_absorb_fire",
+    "channel_absorb_smoulder",
+    "channel_absorb_embers",
 }
+
+--------------------------------------------------------------------------
+
+local SNUFF_ONEOF_TAGS = { "smolder", "fire", "willow_ember" }
+local SNUFF_NO_TAGS = { "INLIMBO","snuffed" }
+local ABSORB_RANGE = 2.5
+
+local function UpdateSnuff(inst, owner)
+	local x, y, z = owner.Transform:GetWorldPosition()
+	for i, v in ipairs(TheSim:FindEntities(x, 0, z, ABSORB_RANGE, nil, SNUFF_NO_TAGS, SNUFF_ONEOF_TAGS)) do
+		if v:IsValid() and not v:IsInLimbo() then
+            local fx = nil
+            local giveember = nil
+			if v:HasTag("willow_ember") then
+                v:AddTag("snuffed")				
+                fx = "channel_absorb_embers"
+                giveember = true
+			elseif v.components.burnable then
+				if v.components.burnable:IsBurning() then
+					v.components.burnable:Extinguish()                    
+                    fx = "channel_absorb_fire"
+				elseif v.components.burnable:IsSmoldering() then
+					v.components.burnable:SmotherSmolder()
+                    fx = "channel_absorb_smoulder"
+				end
+			end
+
+            if fx then
+                owner.SoundEmitter:PlaySound("meta3/willow_lighter/ember_absorb")
+                local fxprefab = SpawnPrefab(fx)
+                fxprefab.Follower:FollowSymbol(owner.GUID, "swap_object", 56, -40, 0)
+
+                if giveember then
+                    v.AnimState:PlayAnimation("idle_pst")
+                    v:DoTaskInTime(10*FRAMES,function()
+                        if not owner.components.health:IsDead() then
+                            owner.components.inventory:GiveItem(v, nil, owner:GetPosition())
+                        end
+                        v:RemoveTag("snuffed")
+                        v.AnimState:PlayAnimation("idle_pre")
+                        v.AnimState:PushAnimation("idle_loop",true)
+                    end)
+                end
+            end
+
+		end
+	end
+end
+
+local function OnStartChanneling(inst, user)
+	if inst.snuff_task then
+		inst.snuff_task:Cancel()
+	end
+	inst.snuff_task = inst:DoPeriodicTask(0.3, UpdateSnuff, nil, user)
+
+	user.SoundEmitter:PlaySound("meta3/willow_lighter/lighter_absorb_LP","channel_loop")
+
+	if inst.snuff_fx then
+		inst.snuff_fx:KillFX()
+	end
+	inst.snuff_fx = SpawnPrefab("channel_absorb_fire_fx")
+	inst.snuff_fx.Follower:FollowSymbol(user.GUID, "swap_object", 56, -40, 0)
+end
+
+local function OnStopChanneling(inst, user)
+
+    user.SoundEmitter:KillSound("channel_loop")
+    user.SoundEmitter:PlaySound("meta3/willow_lighter/extinguisher_deactivate")
+
+	if inst.snuff_task then
+		inst.snuff_task:Cancel()
+		inst.snuff_task = nil
+	end
+	if inst.snuff_fx then
+		inst.snuff_fx:KillFX()
+		inst.snuff_fx = nil
+	end
+end
+
+--------------------------------------------------------------------------
+
+local function applyskillbrightness(inst, value)
+    if inst.fires then
+        for i,fx in ipairs(inst.fires) do
+            fx:SetLightRange(value)
+        end
+    end     
+end
+
+local function removeskillbrightness(inst, value)
+    if inst.fires then
+        for i,fx in ipairs(inst.fires) do
+            fx:SetLightRange(value)
+        end
+    end 
+end
+
+local function applyskillfueleffect(inst,value)
+    inst.components.fueled.rate_modifiers:SetModifier(inst, value,"willowskill")
+end
+local function removeskillfueleffect(inst)
+    inst.components.fueled.rate_modifiers:RemoveModifier(inst, "willowskill")
+end
+
+local function getskillfueleffectmodifier(inst, owner)
+    if not owner.components.skilltreeupdater then
+        return nil
+    end
+end
+
+local function getskillbrightnesseffectmodifier(inst, owner)
+    if not owner.components.skilltreeupdater then
+        return nil
+    end    
+    if owner.components.skilltreeupdater:IsActivated("willow_lightradius_2") then
+        return TUNING.SKILLS.WILLOW_BRIGHTNESS_2
+    elseif   owner.components.skilltreeupdater:IsActivated("willow_lightradius_1") then
+        return TUNING.SKILLS.WILLOW_BRIGHTNESS_1
+    end
+end
+
+local function applylighterskilleffects(inst, data)
+    --SKILLTREE CODE
+    if data.fuelmod then
+        applyskillfueleffect(inst,data.fuelmod)
+    end
+    if data.brightnessmod then
+        applyskillbrightness(inst,data.brightnessmod)
+    end
+end
+
+local function removeskilleffects(inst,brightnessvalue)
+    --SKILLTREE CODE
+    removeskillbrightness(inst, 1)
+    removeskillfueleffect(inst)
+end
+
+local function testforattunedskill(inst,owner)
+    if owner.components.skilltreeupdater:IsActivated("willow_attuned_lighter") then
+        if inst.components.channelcastable == nil then
+            inst:AddComponent("channelcastable")
+            inst.components.channelcastable:SetOnStartChannelingFn(OnStartChanneling)
+            inst.components.channelcastable:SetOnStopChannelingFn(OnStopChanneling)
+        end
+    else
+        inst:RemoveComponent("channelcastable")
+    end
+end
 
 local function onequip(inst, owner)
 local map = TheWorld.Map
@@ -42,6 +194,8 @@ end
 
     owner.SoundEmitter:PlaySound("dontstarve/wilson/lighter_on")
 
+    inst:testforattunedskill(owner)
+
 if naagua == false then
     if inst.fires == nil then
         inst.fires = {}
@@ -57,13 +211,7 @@ if naagua == false then
         end
     end
 end
-
-    --[[if inst.fire == nil then
-        inst.fire = SpawnPrefab("lighterfire")
-        --inst.fire.Transform:SetScale(.125, .125, .125)
-        inst.fire.entity:AddFollower()
-        inst.fire.Follower:FollowSymbol(owner.GUID, "swap_object", 56, -40, 0)
-    end]]
+    applylighterskilleffects(inst, {fuelmod = getskillfueleffectmodifier(inst, owner), brightnessmod = getskillbrightnesseffectmodifier(inst, owner) } )
 end
 
 local function onunequip(inst,owner)
@@ -72,22 +220,49 @@ local function onunequip(inst,owner)
         owner:PushEvent("unequipskinneditem", inst:GetSkinName())
     end
 
-    --[[if inst.fire ~= nil then
-        inst.fire:Remove()
-        inst.fire = nil
-    end]]
     if inst.fires ~= nil then
         for i, fx in ipairs(inst.fires) do
             fx:Remove()
         end
         inst.fires = nil
-        --owner.SoundEmitter:PlaySound("dontstarve/common/fireOut")
     end
 
     inst.components.burnable:Extinguish()
     owner.AnimState:Hide("ARM_carry")
     owner.AnimState:Show("ARM_normal")
     owner.SoundEmitter:PlaySound("dontstarve/wilson/lighter_off")
+
+    removeskilleffects(inst, getskillbrightnesseffectmodifier(inst, owner))
+end
+
+local function applyskilleffect(inst, skill)
+    if skill == "willow_consumption_3" then
+        removeskillfueleffect(inst)
+        applyskillfueleffect(inst,TUNING.SKILLS.WILLOW_CONSUMPTION_3)        
+    elseif skill == "willow_consumption_2" then
+        removeskillfueleffect(inst)
+        applyskillfueleffect(inst,TUNING.SKILLS.WILLOW_CONSUMPTION_2)
+    elseif skill == "willow_consumption_1" then
+        applyskillfueleffect(inst,TUNING.SKILLS.WILLOW_CONSUMPTION_1)
+    end
+
+    if skill == "willow_lightradius_1" then
+        applyskillbrightness(inst,TUNING.SKILLS.WILLOW_BRIGHTNESS_1)
+    elseif skill == "willow_lightradius_2" then
+        removeskillbrightness(inst,TUNING.SKILLS.WILLOW_BRIGHTNESS_1)
+        applyskillbrightness(inst,TUNING.SKILLS.WILLOW_BRIGHTNESS_2)    
+    end
+end
+
+local function onequiptomodel(inst, owner, from_ground)
+    if inst.fires ~= nil then
+        for i, fx in ipairs(inst.fires) do
+            fx:Remove()
+        end
+        inst.fires = nil
+    end
+
+    inst.components.burnable:Extinguish()
 end
 
 local function onpocket(inst, owner)
@@ -96,7 +271,7 @@ end
 
 local function onattack(weapon, attacker, target)
     --target may be killed or removed in combat damage phase
-    if target ~= nil and target:IsValid() and target.components.burnable ~= nil and math.random() < TUNING.LIGHTER_ATTACK_IGNITE_PERCENT * target.components.burnable.flammability then
+    if target ~= nil and target:IsValid() and target.components.burnable ~= nil and (math.random() < TUNING.LIGHTER_ATTACK_IGNITE_PERCENT * target.components.burnable.flammability or attacker.components.skilltreeupdater:IsActivated("willow_controlled_burn_1") ) then
         target.components.burnable:Ignite(nil, attacker)
     end
 end
@@ -105,8 +280,7 @@ local function onupdatefueledraining(inst)
     local owner = inst.components.inventoryitem ~= nil and inst.components.inventoryitem.owner or nil
     inst.components.fueled.rate =
         owner ~= nil and
-        owner.components.sheltered ~= nil and
-        owner.components.sheltered.sheltered and
+		(owner.components.sheltered ~= nil and owner.components.sheltered.sheltered or owner.components.rainimmunity ~= nil) and
         1 or 1 + TUNING.LIGHTER_RAIN_RATE * TheWorld.state.precipitationrate
 end
 
@@ -160,6 +334,17 @@ local function oncook(inst, product, chef)
     elseif inst.components.fueled ~= nil then
         inst.components.fueled:DoDelta(-.01 * inst.components.fueled.maxfuel)
     end
+end
+
+local function OnRemoveEntity(inst)
+	if inst.snuff_fx then
+		inst.snuff_fx:KillFX()
+		inst.snuff_fx = nil
+	end
+end
+
+local function ontakefuel(inst)
+   inst.SoundEmitter:PlaySound("meta3/willow_lighter/ember_absorb")
 end
 
 local function fn()
@@ -216,6 +401,7 @@ local function fn()
     inst.components.equippable:SetOnPocket(onpocket)
     inst.components.equippable:SetOnEquip(onequip)
     inst.components.equippable:SetOnUnequip(onunequip)
+    inst.components.equippable:SetOnEquipToModel(onequiptomodel)
 
     -----------------------------------
 
@@ -232,11 +418,20 @@ local function fn()
     inst.components.fueled:InitializeFuelLevel(TUNING.LIGHTER_FUEL)
     inst.components.fueled:SetDepletedFn(inst.Remove)
     inst.components.fueled:SetFirstPeriod(TUNING.TURNON_FUELED_CONSUMPTION, TUNING.TURNON_FULL_FUELED_CONSUMPTION)
+    inst.components.fueled.fueltype = FUELTYPE.LIGHTER
+    inst.components.fueled.accepting = true
+    inst.components.fueled:SetTakeFuelFn(ontakefuel)
+    
+
+    inst.applyskilleffect = applyskilleffect
+    inst.testforattunedskill = testforattunedskill
 
     inst:WatchWorldState("israining", onisraining)
     onisraining(inst, TheWorld.state.israining)
 
     MakeHauntableLaunch(inst)
+
+	inst.OnRemoveEntity = OnRemoveEntity
 
     return inst
 end
