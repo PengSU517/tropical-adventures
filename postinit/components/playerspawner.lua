9f88017d9a2d05d135ce246f6ec919fe4791bb4a
Peng Su <65644579+PengSU517@users.noreply.github.com>
2024-02-17 12:13:42 +0800

    调整了毁灭季，出生地兼容性调整

--- postinit/components/playerspawner.lua
+++ postinit/components/playerspawner.lua
@@ -11,17 +11,20 @@ AddComponentPostInit("playerspawner", function(self)
         OldSpawnAtLocation(self, inst, player, x, y, z, isloading, ...)
 
         if portal ~= nil then
-            player.Transform:SetPosition(x, y, z)
+            local spawnpoint = FindClosestEntity(player, 2, true, "CLASSIFIED") ------这个函数好用啊
+            if spawnpoint and ((spawnpoint.prefab == "spawnpoint_master") or (spawnpoint.prefab == "spawnpoint_multiplayer")) then
+                player.Transform:SetPosition(x, y, z)
 
-            -------------添加出生物品
-            local startitem
-            if TUNING.tropical.startlocation == "shipwrecked" then
-                startitem = SpawnPrefab("porto_raft_old")
-            elseif TUNING.tropical.startlocation == "hamlet" then
-                startitem = SpawnPrefab("machete")
-            end
-            if startitem then
-                player.components.inventory:GiveItem(startitem)
+                -------------添加出生物品---------------修改为DOTAskintime或许会更好
+                local startitem
+                if TUNING.tropical.startlocation == "shipwrecked" then
+                    startitem = SpawnPrefab("porto_raft_old")
+                elseif TUNING.tropical.startlocation == "hamlet" then
+                    startitem = SpawnPrefab("machete")
+                end
+                if startitem then
+                    player.components.inventory:GiveItem(startitem)
+                end
             end
         end
     end
--- postinit/prefabs/forest.lua
+++ postinit/prefabs/forest.lua
@@ -1,12 +1,12 @@
 AddPrefabPostInit("forest", function(inst)
-    if GLOBAL.TheWorld.ismastersim then
+    if GLOBAL.TheWorld.ismastersim then --------------这是检测服务器还是客户端
         -- inst:AddComponent("parrotspawner") ----不是有birdspawner了吗 -----这个东西很复杂
         inst:AddComponent("economy")
         inst:AddComponent("contador")
 
         inst:AddComponent("twisterspawner")
         inst:AddComponent("roottrunkinventory") ---------------这个是啥啊
-        inst:AddComponent("aporkalypse")
+        -- inst:AddComponent("aporkalypse")
         inst:AddComponent("tropicalgroundspawner")
         inst:AddComponent("tropicalspawner")
         inst:AddComponent("whalehunter")
@@ -15,4 +15,10 @@ AddPrefabPostInit("forest", function(inst)
         inst:AddComponent("rocmanager")
         inst:AddComponent("quaker_interior") ------------这是啥
     end
+end)
+
+AddPrefabPostInit("cave", function(inst)
+    -- inst:AddComponent("economy")
+    -- inst:AddComponent("contador")
+    inst:AddComponent("aporkalypse")
 end)
