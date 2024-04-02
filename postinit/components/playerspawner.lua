fe4d36053d58fb8fb1361ed9d0dd81285c440abc
Peng Su <65644579+PengSU517@users.noreply.github.com>
2024-04-02 12:15:43 +0800

    出生地调整

--- postinit/components/playerspawner.lua
+++ postinit/components/playerspawner.lua
@@ -1,12 +1,12 @@
 AddComponentPostInit("playerspawner", function(self)
     local OldSpawnAtLocation = self.SpawnAtLocation
     function self:SpawnAtLocation(inst, player, x, y, z, isloading, ...)
-        local portal
+        -- local portal
         if not isloading then
-            portal = TheSim:FindFirstEntityWithTag("multiplayer_portal") --  multiplayer_portal  constructionsite
-            if portal ~= nil then
-                x, y, z = portal.Transform:GetWorldPosition()
-            end
+            -- portal = TheSim:FindFirstEntityWithTag("multiplayer_portal") --  multiplayer_portal  constructionsite
+            -- if portal ~= nil then
+            --     x, y, z = portal.Transform:GetWorldPosition()
+            -- end
 
             -------------添加出生物品---------------修改为DOTAskintime或许会更好
             local startitem
@@ -21,11 +21,11 @@ AddComponentPostInit("playerspawner", function(self)
         end
         OldSpawnAtLocation(self, inst, player, x, y, z, isloading, ...)
 
-        if portal ~= nil then
-            local spawnpoint = FindClosestEntity(player, 2, true, "CLASSIFIED") ------这个函数好用啊
-            if spawnpoint and ((spawnpoint.prefab == "spawnpoint_master") or (spawnpoint.prefab == "spawnpoint_multiplayer")) then
-                player.Transform:SetPosition(x, y, z)
-            end
-        end
+        -- if portal ~= nil then
+        --     local spawnpoint = FindClosestEntity(player, 2, true, "CLASSIFIED") ------这个函数好用啊
+        --     if spawnpoint and ((spawnpoint.prefab == "spawnpoint_master") or (spawnpoint.prefab == "spawnpoint_multiplayer")) then
+        --         player.Transform:SetPosition(x, y, z)
+        --     end
+        -- end
     end
 end)
