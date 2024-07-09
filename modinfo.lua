50c89e99d3b1e1eb58b473177deeb26dd5ee0c69
LunarSolar <kageno13927@yeah.net>
2024-07-09 16:43:47 +0800

    仙人掌护甲、制作栏配方相关修改 1. 为WX-78添加了三合一风格的电路配方 2. 将仙人掌护甲的耐久度还原为450 3. 对制作栏配方进行了排序

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ name = en_zh(" Tropical Adventures|Ship of Theseus", "热带冒险|忒修斯之�
 
 
 author = "Peng, 杰杰, Runar"
-version = "2.7.9"
+version = "2.7.9.1"
 forumthread = ""
 api_version = 10
 priority = -10
