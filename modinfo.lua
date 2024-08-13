35859cade6817fa0e10c607daa82ec5d2c3813b1
LunarSolar <kageno13927@yeah.net>
2024-08-13 20:33:09 +0800

    2.8.13

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ name = en_zh(" Tropical Adventures|Ship of Theseus", "热带冒险|忒修斯之�
 
 
 author = "Peng, 杰杰, Runar"
-version = "2.8.10.2"
+version = "2.8.13"
 forumthread = ""
 api_version = 10
 priority = -10
