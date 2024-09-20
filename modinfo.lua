600a78358379126bec89628efeb7bca9f7e29990
Peng <pengsu666@gmail.com>
2024-09-20 23:15:57 +0800

    240920更新

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ name = en_zh(" Tropical Adventures|Ship of Theseus", "热带冒险|忒修斯之�
 
 
 author = "Peng, 杰杰, Runar"
-version = "2.9.11"
+version = "2.9.20"
 forumthread = ""
 api_version = 10
 priority = -100
