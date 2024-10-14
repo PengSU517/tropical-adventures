47e028302a46a07a283521487e5be7d553a5e473
PengSU517 <peng.su@sydney.edu.au>
2024-10-14 23:01:01 +0800

    整理action文件 删除暴食相关的action

--- scripts/languages/language_setting.lua
+++ scripts/languages/language_setting.lua
@@ -1,5 +1,5 @@
 modimport("scripts/languages/stringsEU.lua")
-modimport("scripts/languages/stringscreeps.lua") ---有些没删干净的东西需要这个语言包
+-- modimport("scripts/languages/stringscreeps.lua") ---有些没删干净的东西需要这个语言包
 
 
 if GLOBAL.TUNING.tropical.language == "stringsCH" then
