94a8901688f8beff60118c79afbd9acf27b6d6bf
Peng <pengsu666@gmail.com>
2024-11-17 01:47:47 +0800

    引入单机版的台词
    1，怎么把扩展内容的台词提取出来
    2. 修复正则错配的问题

--- scripts/languages/language_setting.lua
+++
@@ -1,9 +0,0 @@
-modimport("scripts/languages/stringsEU.lua")
--- modimport("scripts/languages/stringscreeps.lua") ---有些没删干净的东西需要这个语言包
-
-
-if GLOBAL.TUNING.tropical.language == "stringsCH" then
-    modimport("scripts/languages/stringsCH.lua")
-elseif GLOBAL.TUNING.tropical.language == "stringsEN" then
-    -- modimport("scripts/languages/stringsCharacters.lua")
-end
