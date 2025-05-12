b36437de6f15b4609d3d8de8619d6e90415f96ee
LunarSolar <kageno13927@yeah.net>
2025-05-12 20:07:50 +0800

    实现上下洞穴时重新连接单向失联的出入口

--- main/postinit.lua
+++ main/postinit.lua
@@ -94,6 +94,7 @@ modimport("postinit/components/soundemitter")
 modimport("postinit/components/thief")
 modimport("postinit/components/unwrappable") ---为批量交易提供支持
 modimport("postinit/components/wavemanager")
+modimport("postinit/components/worldmigrator")
 modimport("postinit/components/worldstate")
 
 --prefabs
