26947cdb9558bf33fc72beddb65bf6a038c3a9c9
LunarSolar <kageno13927@yeah.net>
2025-05-15 21:08:29 +0800

    fix container_replica

--- postinit/components/container_replica.lua
+++ postinit/components/container_replica.lua
@@ -1,7 +1,7 @@
 local Container = require "components/container_replica"
 local Utils = require "tools/utils"
-Utils.FnDecorator(Container, "GetWidget", function(self)
-    return { self.widgetinspect }, not (self.inst.replica.inventoryitem and
-                                        self.inst.replica.inventoryitem:IsHeld() and
-                                        self.widgetinspect)
+Utils.FnDecorator(Container, "GetWidget", nil, function(rets, self)
+    return self.inst.replica.inventoryitem and
+           self.inst.replica.inventoryitem:IsHeld() and
+           self.widgetinspect and { self.widgetinspect } or rets
 end)
