e89395f6cbf6d9cfd66115f11679949f13ea2e02
Peng <pengsu666@gmail.com>
2025-10-05 19:56:26 +0800

    添加mod设置说明

--- modinfo.lua
+++ modinfo.lua
@@ -751,7 +751,14 @@ developer_options =
 
 configuration_options = {}
 
-table_insert(configuration_options, Breaker("Personal Adjustments", "个性化调整"))
+table_insert(configuration_options, Breaker("Client Adjustments", "客户端调整"))
+table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
+table_insert(configuration_options, Breaker("Belows are Client Settings", "以下为客户端设置"))
+table_insert(configuration_options, Breaker("DO NOT WORK ", "“创建游戏”时设置无效"))
+table_insert(configuration_options, Breaker("WHEN HOSTING GAME", ""))
+table_insert(configuration_options, Breaker("please go to Mods setting", "请调整主界面的“模组”设置"))
+table_insert(configuration_options, Breaker("IN THE MAIN MENU", ""))
+table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
 for i, v in my_ipairs(personal_options) do
     table_insert(configuration_options, v)
 end
@@ -768,7 +775,13 @@ end
 --     default = false,
 -- })
 
+
 table_insert(configuration_options, Breaker("Experimental Options", "实验性选项"))
+table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
+table_insert(configuration_options, Breaker("Belows are Experimental Settings", "以下为实验性设置"))
+table_insert(configuration_options, Breaker("PLEASE DISABLE THEM", "如有问题请关闭"))
+table_insert(configuration_options, Breaker("WHEN ENCOUNTERING PROBLEMS", ""))
+table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
 for i, v in my_ipairs(experimental_options) do
     table_insert(configuration_options, v)
 end
@@ -785,7 +798,7 @@ table_insert(configuration_options, Breaker(" ", " "))
 table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
 table_insert(configuration_options, Breaker("Belows are Server Settings", "以下为服务器设置"))
 table_insert(configuration_options, Breaker("DO NOT WORK ", "客户端开服无效"))
-table_insert(configuration_options, Breaker("WITH A CLIENT WORLD", ""))
+table_insert(configuration_options, Breaker("with a client-hosted world", ""))
 table_insert(configuration_options, Breaker("CLIENT PLEASE GOTO", "客户端请调整森林/洞穴设置"))
 table_insert(configuration_options, Breaker("FOREST/CAVE SETTINGS", ""))
 table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
@@ -797,7 +810,7 @@ table_insert(configuration_options, Breaker(" ", " "))
 table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
 table_insert(configuration_options, Breaker("Belows are Server Settings", "以下为服务器设置"))
 table_insert(configuration_options, Breaker("DO NOT WORK ", "客户端开服无效"))
-table_insert(configuration_options, Breaker("WITH A CLIENT WORLD", ""))
+table_insert(configuration_options, Breaker("with a client-hosted world", ""))
 table_insert(configuration_options, Breaker("CLIENT PLEASE GOTO", "客户端请调整森林/洞穴设置"))
 table_insert(configuration_options, Breaker("FOREST/CAVE SETTINGS", ""))
 table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
@@ -809,7 +822,7 @@ table_insert(configuration_options, Breaker(" ", " "))
 table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
 table_insert(configuration_options, Breaker("Belows are Server Settings", "以下为服务器设置"))
 table_insert(configuration_options, Breaker("DO NOT WORK ", "客户端开服无效"))
-table_insert(configuration_options, Breaker("WITH A CLIENT WORLD", ""))
+table_insert(configuration_options, Breaker("with a client-hosted world", ""))
 table_insert(configuration_options, Breaker("CLIENT PLEASE GOTO", "客户端请调整森林/洞穴设置"))
 table_insert(configuration_options, Breaker("FOREST/CAVE SETTINGS", ""))
 table_insert(configuration_options, Breaker("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"))
