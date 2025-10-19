1c40825f31b08c247b2e78853077bdbccf35a843
PengSU517 <pengsu666@gmail.com>
2025-10-19 09:14:59 +0800

    魔力花种植判定

--- scripts/prefabs/waterdrop.lua
+++ scripts/prefabs/waterdrop.lua
@@ -4,18 +4,6 @@ local assets =
     Asset("ANIM", "anim/lifeplant.zip"),
 }
 
-local function IsWater(tile)
-    return tile == GROUND.OCEAN_SWELL or
-        tile == GROUND.OCEAN_BRINEPOOL or
-        tile == GROUND.OCEAN_BRINEPOOL_SHORE or
-        tile == GROUND.OCEAN_HAZARDOUS or
-        tile == GROUND.OCEAN_ROUGH or
-        tile == GROUND.IMPASSABLE or
-        tile == GROUND.OCEAN_COASTAL or
-        tile == GROUND.OCEAN_WATERLOG or
-        tile == GROUND.OCEAN_COASTAL_SHORE
-end
-
 local function oneat(inst, eater)
     if eater.components.poisonable ~= nil then
         eater.components.poisonable:WearOff()
@@ -32,21 +20,6 @@ local function ondeploy(inst, pt)
     inst.planted = true
     inst:Remove()
 end
-
-local notags = { 'NOBLOCK', 'player', 'FX' }
-local function test_ground(inst, pt)
-    local tiletype = TheWorld.Map:GetTile(TheWorld.Map:GetTileCoordsAtPoint(pt:Get()))
-    local ground_OK = tiletype ~= GROUND.ROCKY and tiletype ~= GROUND.ROAD and tiletype ~= GROUND.IMPASSABLE and
-        tiletype ~= GROUND.UNDERROCK and tiletype ~= GROUND.WOODFLOOR and
-        tiletype ~= GROUND.CARPET and tiletype ~= GROUND.CHECKER and tiletype < GROUND.UNDERGROUND and
-        not IsWater(tiletype)
-
-    if ground_OK then
-        return true
-    end
-    return false
-end
-
 
 local function fn(Sim)
     local inst = CreateEntity()
@@ -81,7 +54,8 @@ local function fn(Sim)
     inst.components.edible:SetOnEatenFn(oneat)
 
     inst:AddComponent("deployable")
-    inst.components.deployable.CanDeploy = test_ground
+    inst.components.deployable:SetDeployMode(DEPLOYMODE.PLANT)
+    inst.components.deployable:SetDeploySpacing(DEPLOYSPACING.LESS)
     inst.components.deployable.ondeploy = ondeploy
 
     inst:AddComponent("fuel")
