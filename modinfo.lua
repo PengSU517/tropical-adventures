1a065d3dbcb0e006d112db86029056074467e8cf
BBGoat <bbgoat@vip.qq.com>
2025-12-28 21:44:26 +0800

    hotfix

--- modinfo.lua
+++ modinfo.lua
@@ -64,7 +64,7 @@ name = pub_dev(en_zh("Tropical Adventures|Ship of Theseus", "热带冒险|忒修
     en_zh("Tropical Adventures|Dev", "热带冒险|开发版"))
 
 author = "Peng, Runar, momo, 杰杰"
-version = "3.12.10"
+version = "3.12.10 (hotfix)"
 forumthread = ""
 api_version = 10
 priority = -100
