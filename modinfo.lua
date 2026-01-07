b347e2cfdf3e7e7e538b57b39d3adf3ecb949798
Peng <pengsu666@gmail.com>
2026-01-07 22:35:47 +0800

    hotfix 水面地皮优先级

--- modinfo.lua
+++ modinfo.lua
@@ -64,7 +64,7 @@ name = pub_dev(en_zh("Tropical Adventures|Ship of Theseus", "热带冒险|忒修
     en_zh("Tropical Adventures|Dev", "热带冒险|开发版"))
 
 author = "Peng, Runar, momo, 杰杰"
-version = "4.1.7"
+version = "4.1.7.1"
 forumthread = ""
 api_version = 10
 priority = -100
