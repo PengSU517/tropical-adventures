3f6d7e2f87e79b9101b2a3cf45dcdf74c520ccb8
Peng <pengsu666@gmail.com>
2026-02-24 18:29:04 +0800

    精简船速计算

--- postinit/components/locomotor_boat.lua
+++ postinit/components/locomotor_boat.lua
@@ -23,9 +23,6 @@ local sail_bonus = {
     -- more entries here
 }
 
-local heavybonus = 0.35
-local driftspeed = 2
-
 local function getspeedbonus(inst)
     local val = 1
     if inst.replica.inventory then
@@ -55,74 +52,7 @@ AddComponentPostInit("locomotor", function(self)
         return OldGetSpeedMultiplier(self)
     end
 
-    local OldUpdate = self.OnUpdate
-    function self:OnUpdate(dt)
-        OldUpdate(self, dt)
-        if self.inst:HasTag("aquatic") and self.inst:HasTag("player") and self.inst.replica.inventory
-            and self.inst.replica.inventory:IsHeavyLifting() and not self.driftangle then
-            local item = self.inst.replica.inventory:GetEquippedItem(EQUIPSLOTS.BARCO)
-            if item.replica.container and item.replica.container:GetItemInSlot(1) and
-                item.replica.container:GetItemInSlot(1):HasTag("sail") then
-                if self.inst.Physics:GetMotorSpeed() > 0 then
-                    local desired_speed = self.isrunning and self:RunSpeed() or self.walkspeed
-                    local speed_mult = self:GetSpeedMultiplier()
-                    if self.dest and self.dest:IsValid() then
-                        local destpos_x, destpos_y, destpos_z = self.dest:GetPoint()
-                        local mypos_x, mypos_y, mypos_z = self.inst.Transform:GetWorldPosition()
-                        local dsq = GLOBAL.distsq(destpos_x, destpos_z, mypos_x, mypos_z)
-                        if dsq <= .25 then
-                            speed_mult = math.max(.33, math.sqrt(dsq))
-                        end
-                    end
-
-                    self.inst.Physics:SetMotorVel(desired_speed * speed_mult * heavybonus, 0, 0)
-                end
-            else
-                self.inst.Physics:SetMotorVel(0, 0, 0)
-                self:Stop()
-            end
-        elseif self.driftangle and self.inst:HasTag("player") and self.inst:HasTag("aquatic") then
-            local speed_mult = self:GetSpeedMultiplier()
-            local desired_speed = self.isrunning and self:RunSpeed() or self.walkspeed
-            if self.dest and self.dest:IsValid() then
-                local destpos_x, destpos_y, destpos_z = self.dest:GetPoint()
-                local mypos_x, mypos_y, mypos_z = self.inst.Transform:GetWorldPosition()
-                local dsq = GLOBAL.distsq(destpos_x, destpos_z, mypos_x, mypos_z)
-                if dsq <= .25 then
-                    speed_mult = math.max(.33, math.sqrt(dsq))
-                end
-            end
-            if not self.dest then
-                desired_speed = 0
-            end
-            local angle = self.inst.Transform:GetRotation()
-            local driftx = math.cos(math.rad(-self.driftangle + angle + 180)) * 1.5
-            local drifty = math.sin(math.rad(-self.driftangle + angle + 180)) * 1.5
-
-            local extramult = 1
-
-            if self.inst.replica.inventory and self.inst.replica.inventory:IsHeavyLifting() then
-                extramult = heavybonus
-            end
-
-            self.inst.Physics:SetMotorVel((desired_speed * speed_mult + driftx) * extramult, 0, drifty * extramult)
-            if GLOBAL.StopUpdatingComponents[self] == self.inst then
-                self:StartUpdatingInternal()
-            end
-        end
-    end
-
-    local OldStop = self.Stop
-    function self:Stop(sgparam)
-        OldStop(self, sgparam)
-        if self.driftangle and self.inst:HasTag("aquatic") and self.inst:HasTag("player") and GLOBAL.StopUpdatingComponents[self] == self.inst then
-            self:StartUpdatingInternal()
-        end
-    end
-
     local OldStartHopping = self.StartHopping
-
-
     function self:StartHopping(x, z, target_platform)
         if self.inst:HasTag("aquatic") and self.inst.components.driver then
             self.inst.components.driver:BoatDetached(self.inst)
